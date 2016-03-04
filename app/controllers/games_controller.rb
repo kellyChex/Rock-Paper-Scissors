2ea1ec67c4176a5de1a78f50ee912581950bacf3
KellyChess <chess.kelly@gmail.com>
2016-03-04 13:53:17 -0800

    style changes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,20 @@
 class GamesController < ApplicationController
-
   def new
     @game = Game.new
   end
 
   def create
-    @game = Game.new(player1: game_params[:player1], player2: game_params[:player2])
-      if @game.save
-        if @game.player1_score == 1
-          flash[:notice] = "Player 1 Wins"
-        elsif @game.player2_score == 1
-          flash[:notice] = 'Player 2 Wins!'
-        else
-          flash[:notice] = "Tie"
-        end
-        redirect_to root_path
+    @game = Game.new(player1: game_params[:player1],
+                     player2: game_params[:player2])
+    @game.save
+      if @game.player1_score == 1
+        flash[:notice] = 'Player 1 Wins!'
+      elsif @game.player2_score == 1
+        flash[:notice] = 'Player 2 Wins!'
+      else
+        flash[:notice] = 'Tie'
       end
+      redirect_to root_path
   end
 
   def game_params
